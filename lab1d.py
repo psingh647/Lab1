f9a8dc1426b75b6e52a14c423d4a74e360ba5574
psingh647 <157160740+psingh647@users.noreply.github.com>
2024-09-19 23:24:41 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Define integer objects
+x = 10
+y = 2
+z = 5
+
+# Calculate and print the result
+result = x + y * z  # According to PEMDAS, multiplication is done before addition
+print(f'{x} + {y} * {z} = {result}')
