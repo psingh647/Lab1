32ee461947b5bab9b7b30359aa1ea93cc851f573
psingh647 <157160740+psingh647@users.noreply.github.com>
2024-09-19 23:24:01 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Define string and integer objects
+name = 'Isaac'
+age = 72
+
+# Print the combined string and integer in a sentence
+print(name + ' is ' + str(age) + ' years old!')
